cac5f8943da47519136e6665d22d236c32d1ff6f
Emilian <emiliane@users.noreply.github.com>
2025-03-04 16:27:06 +0200

    Add files via upload

--- off.py
+++ off.py
@@ -1,12 +1,11 @@
 ##https://askubuntu.com/questions/105536/tool-to-shutdown-system-when-there-is-no-network-traffic
-
 ## Download Monitor v0.1 - March 2012
 
 # Set the interface you wish to monitor, eg: eth0, wlan0, usb0
 INTERFACE = "eth0"
 
 # Set the minimum download speed in KB/s that must be achieved.
-MINIMUM_SPEED = 150
+MINIMUM_SPEED = 444
 
 # Set the number of retries to test for the average minimum speed. If the average speed is less
 # than the minimum speed for x number of retries, then shutdown.
@@ -20,35 +19,46 @@ import os, time
 #from commands import getoutput
 
 def worker ():
+    print("Începe lucrătorul.")
     RETRIES_COUNT = RETRIES
     while True:
+        print("-")
         #SPEED = int(float(getoutput("ifstat -i %s 1 1 | awk '{print $1}' | sed -n '3p'" % INTERFACE)))
-        print("Speed is " + getSpeed() + " KB/s.")
+        print("Viteza este de " + getSpeed() + " KB/s.")
         SPEED = int(float(getSpeed()))
-        print(SPEED)
+        #print(SPEED)
         if (SPEED < MINIMUM_SPEED and RETRIES_COUNT <= 0):
-            print("Se oprește!")
+            print("Se oprește imediat!")
             os.system("shutdown -h now")
+            #os.system("shutdown -h +44")
+            break
         elif SPEED < MINIMUM_SPEED:
-            print("Versiunea elif.")
+            print("S-a întâmplat de " + str(RETRIES -RETRIES_COUNT + 1) + " din " + str(RETRIES) + " consecutiv ca viteza să fie mai mică decât " + str(MINIMUM_SPEED) + "KB/s.")
             RETRIES_COUNT -= 1
-            print("Doarme pentru " + str(INTERVAL) + " secunde.")
+            print("Așteaptă pentru " + str(INTERVAL) + " secunde.")
             time.sleep(INTERVAL)
         else:
-            print("Versiunea else.")
+            print("Mai încearcă.")
             RETRIES_COUNT = RETRIES
             print("Doarme pentru " + str(INTERVAL) + " secunde.")
             time.sleep(INTERVAL)
 
-file = "speed.txt"
 
-def getSpeed ():
+def getSpeed():
+    file = "speed.txt"
     f = open(file, "r")
     speed = f.read()
     return str(speed)
 
-while (True):
-    try:
-        worker()
-    except:
-        print("An exception occurred.")
+def bucla():
+    while (True):
+        print("Începe bucla.")
+        try:
+            worker()
+        except:
+            print("A avut loc o erare.")
+            print("Reîncepe bucla după " + str(INTERVAL) + " secunde.")
+            time.sleep(INTERVAL)
+
+#worker()
+bucla()
